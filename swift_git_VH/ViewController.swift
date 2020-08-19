b6f5cc92cdbccecc9b84c203c92ea82d3c6c581b
Viktor Holovach <viktorholovach@gmail.com>
2020-08-19 16:39:21 +0300

    Initial commit

---
+++ swift_git_VH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift_git_VH
+//
+//  Created by Viktor Golovach on 19.08.2020.
+//  Copyright © 2020 Viktor Golovach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
