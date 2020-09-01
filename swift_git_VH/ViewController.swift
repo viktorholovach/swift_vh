a2443e04053d31bc284f0c19e53390624900f815
Viktor Holovach <viktorholovach@gmail.com>
2020-09-01 17:08:07 +0300

    Assignment 3 Task 3

--- swift_git_VH/ViewController.swift
+++ swift_git_VH/ViewController.swift
@@ -15,28 +15,80 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         
-        reverseString()
-        
-        
-        
-        
+       nameShift()
+        secondVersion()
     }
     
     
-    func reverseString() {
-        let str = "Mountains"
-        var reversedStr = ""
-        for char in str {
-            reversedStr = "\(char)" + reversedStr
+    func nameShift() {
+        let name = "OliverKing"
+        var splitName = name
+        var indexCapital: [Int] = []
+        for (index, element) in name.enumerated() {
+            if element.isUppercase {
+                indexCapital.append(index)
+            }
         }
-        print(reversedStr)
+        
+        let indexToSplit: Int = indexCapital.last!
+        
+        splitName.insert(" ", at: splitName.index(splitName.startIndex, offsetBy: indexToSplit))
+        
+        let nameOnly = name.prefix(indexToSplit)
+        let startfathersName = name.index(name.startIndex, offsetBy: indexToSplit)
+        let finnishFathersName = name.endIndex
+        let fathersName = startfathersName..<finnishFathersName
+        let myString = name[fathersName]
         
         
+        
+        print(splitName)
+        print(nameOnly)
+        print(myString)
         
         
         
     }
+    
+    func secondVersion() {
+        let name = "ViktorHolovach"
+        var fullName = ""
+        var nameOnly = ""
+        var fathersName = ""
+        for char in name {
+            if char.isUppercase {
+                fullName += " " + String(char)
+                fathersName = String(char)
+                nameOnly += String(char)
+            }
+            if char.isLowercase {
+                fullName += String(char)
+                fathersName += String(char)
+            }
+        }
+        
+        fullName.remove(at: fullName.startIndex)
+        print(fullName)
+        print(fathersName)
+        print("\(nameOnly) cant pull name =( ")
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
+
 
 
 
